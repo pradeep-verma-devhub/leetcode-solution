3311aefddfacea2716f1b7032d289e94df849c83
Pradeep Verma <pradeep.verma.devhub@gmail.com>
2026-08-13 16:01:20 +0000

    [LeetCode Sync] Runtime - 17 ms (78.01%), Memory - 45.8 MB (14.61%)

---
+++ 0016-3sum-closest/solution.java
@@ -0,0 +1,27 @@
+class Solution{
+    public int threeSumClosest(int[] nums, int target){
+        Arrays.sort(nums);
+        int assume = nums[0] + nums[1] + nums[2];
+        for(int i = 0 ; i < nums.length - 2 ; i++){
+            int j = i + 1;
+            int k = nums.length - 1;
+            while(j < k){
+                int sum = nums[i] + nums[j] + nums[k];
+                if(Math.abs(sum - target) < Math.abs(assume - target)){
+                      assume = sum;
+                }
+                if(sum < target){
+                    j++;
+                }
+                else if(sum > target){
+                    k--;
+                }
+                else{
+                    assume = sum;
+                    break;
+                }
+            }
+        }
+        return assume;
+    }
+}
